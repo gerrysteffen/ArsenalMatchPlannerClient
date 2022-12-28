1f6b4866a13e2e578ad53ae6c7a6fc4e1455cb9a
Gerry Steffen <g_steffen@hotmail.com>
2022-12-28 12:23:40 +0100

    feat: api service fetching matches from server

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import { useEffect, useState } from "react";
+import { fetchMatches } from "./api-client";
 import "./App.css";
 
 function App() {
-  const [games, setGames] = useState([])
+  const [matches, setMatches] = useState([])
   const [reservedTickets, setResTics] = useState([])
 
-  
-
-  const gamesSampleData = [
-    {
-      date: '15/01/23',
-      team: 'Tottenham'
-    },
-    {
-      date: '15/01/23',
-      team: 'Tottenham'
-    },
-  ]
+  useEffect(()=> {
+    const getMatches = async () => {
+      const matches =  await fetchMatches()
+      matches.map(match => {
+        const date = new Date(match.timestamp*1000).toLocaleString('en-GB', {timeZone: 'Europe/London'})
+        match.date = date
+        match.shortDate = match.date.slice(0,10)
+      })
+      console.log(matches)
+      setMatches(matches)
+    }
+    getMatches()
+  },[])
 
   return (
     <div className="App">
       <div className="Header">Header</div>
       <div className="Body">
         <div className="dropdown">
-          <select name='game' id='game-select'>
+          <select name='match' id='match-select'>
             <option value=''>--please select a game--</option>
-            {gamesSampleData && gamesSampleData.map(data => {
-              return <option>{data.date}: {data.team}</option>
+            {matches.length > 0 && matches.map(data => {
+              return <option>{data.shortDate} - {data.tournament}, round {data.round}: <strong>{data.homeTeamShort} vs. {data.awayTeamShort}</strong></option>
             })}
           </select>
         </div>
--- src/api-client.js
+++ src/api-client.js
@@ -1,1 +1,7 @@
+const url = 'http://localhost:3001';
 
+export const fetchMatches = async () => {
+  const res = await fetch(url + '/matches').catch((error) => console.log(error));
+  console.log(res)
+  return res.json();
+};
